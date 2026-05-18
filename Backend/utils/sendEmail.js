b9ae3cf2b2bd5519f5204e40e91cb96832549011
DEEPAK MEHTA <77485514+Deepak052@users.noreply.github.com>
2026-05-18 17:20:23 +0530

    Update sendEmail.js

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -6,10 +6,14 @@ const sendEmail = async ({ to, subject, html }) => {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         },
+        family: 4, // Force IPv4
         connectionTimeout: 10000, // 10 seconds timeout
         greetingTimeout: 5000,
         socketTimeout: 10000
