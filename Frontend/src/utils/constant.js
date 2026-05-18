096b106a0d6493e875c50e512410670b5519c97c
DEEPAK MEHTA <77485514+Deepak052@users.noreply.github.com>
2026-05-18 17:14:01 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const Backend_URL = 'https://learnlog-0k6m.onrender.com';
+export const Backend_URL = 'https://learnlog-0k6m.onrender.com/api';
 // export const Backend_URL = 'http://localhost:3000/api';
